d3f286f1c433187d9dcd9ffa31693aeb18647354
balaram <balaram.rudra@gmail.com>
2017-03-15 02:49:56 +0530

    ng-src example

--- AngularJSTutorial/Scripts/Scripts.js
+++ AngularJSTutorial/Scripts/Scripts.js
@@ -1,6 +1,8 @@
 ﻿/// <reference path="angular.min.js" />
-var myApp = angular.module("myModule", []);
-
-myApp.controller("myController", function ($scope) {
-    $scope.message = "Hello, Vijay !";
+var myApp = angular.module("myModule", []).controller("myController", function ($scope) {
+    $scope.country = {
+        name: "Balarama",
+        capital: "Rudraraju",
+        flag: "/Images/TV.gif"
+    };
 });
